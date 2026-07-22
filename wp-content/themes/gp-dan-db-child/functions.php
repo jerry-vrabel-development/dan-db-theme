15a43074e834ffc64e7897e214cf09b3113c2dad
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2026-07-22 13:54:27 -0500

    Initial commit: GeneratePress child theme + article cover images for dan-db.com

---
+++ wp-content/themes/gp-dan-db-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * GP Dan DB Child theme functions.
+ *
+ * GeneratePress automatically enqueues both the parent and child
+ * style.css, so no manual wp_enqueue_style is needed here.
+ * This file is for custom hooks, filters, and PHP.
+ */
